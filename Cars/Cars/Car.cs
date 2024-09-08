b0308df30a346f2d2ce6fdf5a9f8af523c94f937
Борислав Христов <hristovborislav45@gmail.com>
2024-09-08 16:55:35 +0300

    Add Program and Car

---
+++ Cars/Cars/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Car : Engine
+    {
+        public string? Brand { get; set; }  // public property
+        public string? Model { get; set; }  // public property
+
+        public Car(string brand, string model)  // constructor that sets brand and model
+        {
+            Brand = brand;
+            Model = model;
+        }
+    }
+}
---
+++ Cars/Cars/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Cars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car toyota = new Car("Toyota", "Camry"); // create new car
+            Console.WriteLine("{0}, {1} - Engine ID: {2}", toyota.Brand, toyota.Model, toyota.GetEngineId()); // format string
+            toyota.Rev(); // call Rev() method
+            
+        }
+    }
+}
